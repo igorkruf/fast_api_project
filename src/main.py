ff39488c957188341d1930079c9eb268b440ad37
unknown <igor.kruf@gmail.com>
2025-06-08 17:43:43 +0300

    изменил main.py добавил запуск приложения в нём

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -7,3 +9,6 @@ app = FastAPI()
 def read_root():
     html_content = "<h2>Hello METANIT.COM!</h2>"
     return HTMLResponse(content=html_content)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
